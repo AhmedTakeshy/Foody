d7e8eb5e840a101bc154d7705807b0e130cb6175
Ahmed Abdelsamie <Ahmedtakeshy7@gmail.com>
2023-06-24 16:30:52 +0300

    Updating the new style of products

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,6 +1,7 @@
 import { cartActions } from "../store/cart-slice";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
+import { MdInfoOutline } from "react-icons/md";
 
 const Product = ({ img, price, title, id, calories, ingredients, type }) => {
   const dispatch = useDispatch();
@@ -23,7 +24,7 @@ const Product = ({ img, price, title, id, calories, ingredients, type }) => {
   }
 
   return (
-    <div className=" w-[300px] m-5 border border-[#ccc] p-5 text-center rounded-md hover:scale-105 transition duration-500 hover:shadow-md">
+    <div className=" w-[300px] m-5 border border-[#ccc] p-5 h-[40rem] text-center rounded-md hover:scale-105 transition duration-500 hover:shadow-md">
       <img
         src={`${img.includes("https") ? img : "http://localhost:3000/" + img} `}
         alt={title}
@@ -32,29 +33,32 @@ const Product = ({ img, price, title, id, calories, ingredients, type }) => {
       <h2 className="text-[1.4rem] mb-2 font-bold">{title}</h2>
       <p className="mb-3 text-xl text-primary">{newPrice || `₺${price}`}</p>
       <div className="flex flex-col items-center justify-center gap-2">
-        <button
-          onClick={addHandler}
-          className="px-8 py-3 text-xl text-white border-none rounded-full bg-primary hover:bg-secondary"
-        >
-          Sepete Ekle
-        </button>
-        {type !== "drinks" && <button
-          onClick={handleIngredients}
-          className="px-8 py-3 text-xl text-white border-none rounded-full bg-primary hover:bg-secondary"
-        >
-          İçindekiler
-        </button>}
-      </div>
-      {showIngredients &&
-        <div className="flex flex-col items-center justify-center mt-4">
-          <ul className="flex flex-col list-disc ">
+        <div className="flex items-center justify-between">
+          <button
+            onClick={addHandler}
+            className="px-8 py-3 mr-3 text-xl text-white border-none rounded-full bg-primary hover:bg-secondary"
+          >
+            Sepete Ekle
+          </button>
+          <MdInfoOutline className="cursor-pointer peer" size={20} />
+          <p className="relative hidden mt-4 text-xl peer-hover:inline-block">
+            <span className="absolute w-32 h-20 py-2 text-white rounded-md -left-5 bottom-4 bg-slate-800">
+              Kalori: ~{calories}kcal
+            </span>
+          </p>
+
+        </div>
+        {type !== "drinks" && <div className="flex flex-col items-center justify-center mt-4">
+          <ul className="flex flex-col">
             {ingredients?.map((ingredient, index) => (
               <li key={index} className="text-xl">{ingredient}</li>
             ))}
           </ul>
-          <p className="mt-4 text-2xl"><b>Kalori: </b>~{calories}kcal</p>
-        </div>
-      }
+        </div>}
+      </div>
+
+
+
     </div>
   );
 };
