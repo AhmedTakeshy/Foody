1d75410e5085edfc0da818d46e1ead57ad58cb03
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2023-07-19 01:55:01 +0300

    Fix waiter page in admin route

--- src/pages/WaiterPage.jsx
+++ src/pages/WaiterPage.jsx
@@ -4,10 +4,11 @@ const WaiterPage = () => {
 
     const adminData = useOutletContext();
     const { waiter } = adminData;
+    const waiterArray = Object.values(waiter);
 
     return (
         <div className='grid items-center gap-4 mt-12 lg:grid-cols-4 md:grid-cols-3'>
-            {waiter.map(w => (
+            {waiterArray.map(w => (
                 <div key={w.id} className="max-w-md p-2 rounded-3xl bg-gradient-to-b from-blue-300 to-pink-300 dark:from-blue-800 dark:to-purple-800 ">
                     <div className="rounded-[calc(1.5rem-.5rem)] p-6 bg-white dark:bg-gray-900">
                         <div className="flex flex-col items-start">
